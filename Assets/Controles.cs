1ccb0a4e87900d15e298a379ef08f033e0b3a841
Alberto Pedron <pedron.albert@gmail.com>
2015-11-15 03:17:45 -0430

    Initial commit

---
+++ Assets/Controles.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controles : MonoBehaviour {
+	public float velocidadMov;
+	public float fuerzaSalto;
+	public bool isGrounded = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		//Moverse
+		float axisX = Input.GetAxis("Horizontal");
+		Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+		
+		rb.velocity = new Vector2(axisX * this.velocidadMov, rb.velocity.y);
+		
+		//Saltar
+		bool saltar = Input.GetKey(KeyCode.Space);
+		
+		if(saltar && this.isGrounded) {
+			this.isGrounded = false;
+			rb.AddForce(new Vector2(0F, fuerzaSalto), ForceMode2D.Impulse);;
+		}
+
+	}
+
+	void OnTriggerStay2D(Collider2D colision) {
+		if (colision.gameObject.tag == "floor") {
+			this.isGrounded = true;
+		} else if (colision.gameObject.tag == "tube") {
+			this.isGrounded = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D colision) {
+		if (colision.gameObject.tag == "floor") {
+			this.isGrounded = false;
+		} else if (colision.gameObject.tag == "tube") {
+			this.isGrounded = false;
+		}
+	}
+
+}
---
+++ Assets/FollowMario.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMario : MonoBehaviour {
+	public GameObject Mario;
+	public float leftOffset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float marioPosX = Mario.transform.position.x;
+
+		this.transform.position = new Vector3(
+			marioPosX - this.leftOffset,
+			this.transform.position.y,
+			this.transform.position.z
+		);
+	}
+}
